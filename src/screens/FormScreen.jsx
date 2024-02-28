2e8209387bd9128923444ea69320bb3ee0fb8e2b
Evangelene <siyinyap@gmail.com>
2024-02-28 21:25:50 +0800

    Initial commit

---
+++ src/screens/FormScreen.jsx
@@ -0,0 +1,13 @@
+import { Text, View, SafeAreaView } from "react-native";
+
+export default function FormScreen() {
+  return (
+    <SafeAreaView
+      style={{ width: "100%", height: "100%", backgroundColor: "#F4F4F4" }}
+    >
+      <View>
+        <Text>This is a form page!!</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
