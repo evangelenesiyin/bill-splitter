709e71dc21f176a17c23e4eb0a3ecc8f761beadd
Evangelene <siyinyap@gmail.com>
2024-02-29 00:53:00 +0800

    Initial commit

---
+++ src/components/FormPeople.jsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native"
+
+export default function FormPeople() {
+
+    return (
+        <Text>Who's sharing?</Text>
+    )
+}
---
+++ src/components/FormReceipt.jsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native"
+
+export default function FormReceipt() {
+
+    return (
+        <Text>Bill breakdown</Text>
+    )
+}
