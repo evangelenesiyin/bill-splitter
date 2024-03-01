9c7048f76bea14f6e8a34253585118603bea95d5
Evangelene <siyinyap@gmail.com>
2024-03-02 01:05:49 +0800

    feat: add custom css to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,23 @@ module.exports = {
       fontFamily: {
         trirong: ["Trirong_400Regular"],
         trispace: ["Trispace_100Thin"],
+        roboto: ["Roboto"],
         robotocondensed: ["RobotoCondensed_400Regular"],
       },
       colors: {
         gray: "#F4F4F4",
+        midgray: "#939393",
+        darkgray: "#595C6C",
         darknavy: "#161A30",
         navy: "#31304D",
         beige: "#F0ECE5",
         green: "#60EBA9",
         red: "#D95A5A",
         yellow: "#F4E773",
+      },
+      fontSize: {
+        title: "90px",
+        titleslash: "110px",
       },
     },
   },
