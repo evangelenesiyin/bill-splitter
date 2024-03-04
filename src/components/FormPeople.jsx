bbe466da15325db363beaf09e4a2ece5aa1c8d46
Evangelene <siyinyap@gmail.com>
2024-03-05 00:07:13 +0800

    style: add styling/UI to FormPeople section

--- src/components/FormPeople.jsx
+++ src/components/FormPeople.jsx
@@ -1,5 +1,7 @@
 import { Text, View } from "react-native";
 import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
 
 export default function FormPeople() {
 
@@ -12,9 +14,16 @@ export default function FormPeople() {
     }
 
     return (
-        <View className="ml-8">
-        <Text className="font-roboto text-xl">Who's sharing?</Text>
-        
+        <>
+        <View className="flex flex-row ml-8">
+        <Text className="font-roboto text-xl text-darknavy">Who's sharing?</Text>
+        <View className="flex flex-1 items-end mr-8 justify-center">
+        <FontAwesomeIcon icon={ faPlus } />
         </View>
+        </View>
+        <View className="ml-8 mt-2">
+            <Text className="text-midgray">No names added</Text>
+        </View>
+        </>
     )
 }
