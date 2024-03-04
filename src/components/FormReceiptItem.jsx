2b22291522147a50bae050a9f98032da2f3a2487
Evangelene <siyinyap@gmail.com>
2024-03-05 00:08:16 +0800

    style: add styling to FormReceiptItem

--- src/components/FormReceiptItem.jsx
+++ src/components/FormReceiptItem.jsx
@@ -1,6 +1,7 @@
 import { Pressable, Text, View } from "react-native";
 import { useSetAtom } from "jotai";
 import { indexToEditAtom } from "../utils/atom";
+import { useFonts, Roboto_100Thin, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function FormReceiptItem({
   index,
@@ -14,11 +15,26 @@ export default function FormReceiptItem({
     setEditReceiptItemModalVisible(true);
   };
 
+  const [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <Pressable onPress={handleReceiptItemPress}>
-      <Text>{receiptItem.item_name}</Text>
-      <Text>{parseFloat(receiptItem.item_price).toFixed(2)}</Text>
-      <Text>{receiptItem.item_sharers.join(", ")}</Text>
+      <View className="flex flex-row">
+      <Text className="font-roboto text-base text-darkgray">{receiptItem.item_name}</Text>
+      <View className="flex flex-1 items-end mr-8">
+      <Text className="font-roboto text-base text-darkgray">${parseFloat(receiptItem.item_price).toFixed(2)}</Text>
+      </View>
+      </View>
+      <Text className="font-roboto text-base text-midgray mb-4">{receiptItem.item_sharers.join(", ")}</Text>
     </Pressable>
   );
 }
