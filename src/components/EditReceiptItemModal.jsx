fe39a1282045bb38e1533daee43f4e88959ebd7b
Evangelene <siyinyap@gmail.com>
2024-03-24 01:17:33 +0800

    feat: add modal for Bill Breakdown section

--- src/components/EditReceiptItemModal.jsx
+++ src/components/EditReceiptItemModal.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import { useAtomValue, useAtom } from "jotai";
-import { Button, Modal, Pressable, Text, TextInput, View } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
+import Dialog, { DialogContent } from "react-native-popup-dialog";
 import { indexToEditAtom, receiptItemsAtom, sharersAtom } from "../utils/atom";
 import { receiptItemTemplate } from "../data/template";
+import { faXmark } from "@fortawesome/free-solid-svg-icons/faXmark";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 export default function EditReceiptItemModal({
   editReceiptItemModalVisible,
@@ -98,53 +101,79 @@ export default function EditReceiptItemModal({
   }, [editReceiptItemModalVisible]);
 
   return (
-    <Modal
+    <Dialog
       visible={editReceiptItemModalVisible}
       onRequestClose={handleCloseModal}
+      onTouchOutside={handleCloseModal}
     >
-      <View>
-        {isNew ? <Text>Enter Details</Text> : <Text>Edit details</Text>}
-        <Pressable onPressOut={handleCloseModal}>
-          <Text>x</Text>
-        </Pressable>
-      </View>
+      <DialogContent className="bg-beige w-80">
+    <View className="flex-row items-center justify-between">
+      <Text className="font-robotomedium pt-5 pb-2 px-1 text-base">
+        {isNew ? "Enter details" : "Edit details"}
+      </Text>
+      <Pressable onPressOut={handleCloseModal}>
+        <FontAwesomeIcon icon={faXmark} />
+      </Pressable>
+    </View>
 
-      <Text>Description (optional)</Text>
+      <Text className="font-roboto pb-2 px-1 text-sm">Description (optional)</Text>
       <TextInput
         value={receiptItem.item_name}
         onChangeText={(text) => handleChangeItemName(text)}
+        className="bg-gray border border-midgray rounded mb-2 mx-1 py-2 px-3"
       />
 
-      <Text>Total item amount (SGD)</Text>
+      <Text className="font-roboto pb-2 px-1 text-sm">Total item amount (SGD)</Text>
       <TextInput
         value={String(receiptItem.item_price)}
         onChangeText={(text) => handleChangeItemPrice(text)}
         keyboardType="numeric"
         inputType="decimal"
+        className="bg-gray border border-midgray rounded mb-2 mx-1 py-2 px-3"
       />
 
-      <Text>Allocate portion(s)</Text>
-      <Text>Select multiple names if shared by a few people</Text>
+      <Text className="font-roboto mt-1 mx-1 text-sm">Allocate portion(s)</Text>
+      <Text className="font-roboto mb-2 mx-1 text-xs">Select multiple names if shared by a few people</Text>
+      <View className="flex flex-row flex-wrap mb-3">
+      {sharers.length === 0 && (
+        <Text className="font-roboto text-xs text-midgray ml-1 mb-1">No names added</Text>
+      )}
       {sharers.map((sharer, index) => {
+        const isSelected = receiptItem.item_sharers?.includes(sharer);
         return (
           <Pressable
             key={index}
             onPressOut={() => handleAddRemoveSharer(sharer)}
+            className={`border rounded-full py-1 px-3 m-0.5 ${
+              isSelected ? 'border-darknavy bg-darknavy text-beige' : 'border-midgray bg-gray'
+            }`}
           >
-            <Text>
-              {sharer} {receiptItem.item_sharers?.includes(sharer) && "o"}
-            </Text>
+            <Text className="text-center">{sharer}</Text>
           </Pressable>
         );
       })}
+    </View>
+
       {isNew ? (
-        <Button title="ADD" disabled={!isComplete} onPress={handleAdd} />
+        <Pressable>
+        <Text disabled={!isComplete} onPress={handleAdd} className="font-robotocondensed py-1 px-3 mx-16 text-lg text-center bg-darknavy text-beige"
+        style={{ borderRadius: 15, overflow: 'hidden' }}>ADD</Text>
+        </Pressable>
       ) : (
-        <View>
-          <Button title="SAVE" disabled={!isComplete} onPress={handleEdit} />
-          <Button title="DELETE" onPress={handleDelete} />
-        </View>
+        <View className="flex-row justify-center">
+          <Pressable>
+          <Text 
+          title="DELETE" onPress={handleDelete} className="font-robotocondensed py-1 px-10 m-1 text-lg text-center bg-red text-beige rounded-full" 
+          style={{ borderRadius: 15, overflow: 'hidden' }}>DELETE</Text>
+          </Pressable>
+        <Pressable>
+          <Text 
+          disabled={!isComplete} onPress={handleEdit} className="font-robotocondensed py-1 px-12 m-1 text-lg text-center bg-darknavy text-beige rounded-full" 
+          style={{ borderRadius: 15, overflow: 'hidden' }}>SAVE</Text>
+          </Pressable>
+          </View>
       )}
-    </Modal>
+      </DialogContent>
+    </Dialog>
   );
 }
